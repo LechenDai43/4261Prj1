b948afbabaf25f176e89b60498866cc78efd9466
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2021-02-09 06:09:12 -0500

    something more

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -6,6 +6,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
@@ -22,11 +24,15 @@ import android.widget.Toolbar;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
@@ -34,6 +40,8 @@ import java.util.ArrayList;
 public class Home extends AppCompatActivity {
     String username = "", email = "";
     EditText searchBarTextFieldHm;
+    ImageButton notificationButtonHm1;
+    ImageButton notificationButtonHm2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,18 +54,23 @@ public class Home extends AppCompatActivity {
 
         searchBarTextFieldHm = (EditText) findViewById(R.id.searchBarTextFieldHm);
         ImageButton searchButtonHm = (ImageButton) findViewById(R.id.searchButtonHm);
-        ImageButton notificationButtonHm = (ImageButton) findViewById(R.id.notificationButtonHm);
+        notificationButtonHm1 = (ImageButton) findViewById(R.id.notificationButtonHm);
+        notificationButtonHm2 = (ImageButton) findViewById(R.id.notificationButtonHm2);
         Button logOutButtonHm = (Button) findViewById((R.id.logOutButtonHm));
 
         logOutButtonHm.setOnClickListener(new LogoutListener());
         searchButtonHm.setOnClickListener(new SearchFriendListener(this));
-        notificationButtonHm.setOnClickListener(new OpenNotificationListener());
+        notificationButtonHm1.setOnClickListener(new OpenNotificationListener());
+        notificationButtonHm2.setOnClickListener(new OpenNotificationListener());
 
         RenderFriend rf = new RenderFriend();
         rf.searchBarTextFieldHm = searchBarTextFieldHm;
         rf.context = this;
         rf.myListView = (ListView)findViewById(R.id.listViewHm);
         rf.execute("");
+
+        notificationButtonHm2.setVisibility(View.GONE);
+        notificationButtonHm1.setVisibility(View.VISIBLE);
     }
 
     class OpenNotificationListener implements View.OnClickListener {
@@ -173,6 +186,77 @@ public class Home extends AppCompatActivity {
             }
             else if (type.equals("inviting")) {
 
+            }
+        }
+    }
+
+    class CheckNotification extends AsyncTask<String, String, String> {
+        String testee = "0";
+
+        @Override
+        protected String doInBackground(String... strings) {
+            while (true) {
+                if (testee.equals("0")) {
+                    DetailCheck dc = new DetailCheck();
+                    dc.execute();
+                }
+            }
+        }
+
+        class DetailCheck extends AsyncTask<String, String, String> {
+            boolean checked = false;
+            @Override
+            protected void onPreExecute() {
+                testee = "1";
+            }
+
+            @Override
+            protected void onPostExecute(String args) {
+                testee = "0";
+                if (checked) {
+                    notificationButtonHm1.setVisibility(View.GONE);
+                    notificationButtonHm2.setVisibility(View.VISIBLE);
+                }
+                else {
+                    notificationButtonHm1.setVisibility(View.VISIBLE);
+                    notificationButtonHm2.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            protected String doInBackground(String... strings) {
+                try {
+                    URL url = new URL(GlobalVariables.ADDRESS + "friends.php");
+                    HttpURLConnection httpURLConnection = null;
+                    httpURLConnection = (HttpURLConnection) url.openConnection();
+                    httpURLConnection.setRequestMethod("POST");
+                    httpURLConnection.setDoInput(true);
+                    httpURLConnection.setDoOutput(true);
+
+                    OutputStream outputStream = httpURLConnection.getOutputStream();
+                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                    String post_data = URLEncoder.encode("f3", "UTF-8") + "=" + URLEncoder.encode("true", "UTF-8") + "&" +
+                            URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8");
+
+                    bufferedWriter.write(post_data);
+                    bufferedWriter.flush();
+                    bufferedWriter.close();
+                    outputStream.close();
+
+                    InputStream inputStream =  httpURLConnection.getInputStream();
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
+                    String line = bufferedReader.readLine();
+                    checked = line.equals("On");
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                } catch (ProtocolException e) {
+                    e.printStackTrace();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
             }
         }
     }
