559483b41c1c7990609a1e9f8c782ec14e27c053
qchen336 <qchen336@gatech.edu>
2021-02-08 20:27:52 -0500

    update xml

--- app/src/main/java/com/example/myapplication/Forget.java
+++ app/src/main/java/com/example/myapplication/Forget.java
@@ -2,13 +2,51 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-public class Forget extends AppCompatActivity {
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+  public class Forget extends AppCompatActivity{
+//public class Forget extends AppCompatActivity implements View.OnClickListener{
+    EditText emailTextFieldFg;
+    Button sendEmailButtonFg, returnLoginFg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget);
+
+//        emailTextFieldFg = (EditText)findViewById(R.id.emailTextFieldFg);
+//        sendEmailButtonFg = (Button) findViewById(R.id.sendEmailButtonFg);
+//        returnLoginFg = (Button) findViewById(R.id.returnLoginFg);
+
     }
+
+//    @Override
+//    public <MyActivity> void onClick(View view) {
+//        if(emailTextFieldFg.equals("") || emailTextFieldFg == null) {
+//            Toast.makeText(getApplicationContext(), "Email Address Empty", Toast.LENGTH_SHORT).
+//        }
+//            switch (view.getId()) {
+//                case R.id.sendEmailButtonFg:
+//                    Intent i = new Intent(Intent.ACTION_SEND);
+//                    i.setType("text/plain");
+//                    i.putExtra(Intent.EXTRA_EMAIL, new String[]{"recipient@example.com"});
+//                    i.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+//                    i.putExtra(Intent.EXTRA_TEXT, "Your temporary passcode is 12423444");
+//                    try {
+//                        startActivity(Intent.createChooser(i, "Send mail..."));
+//
+//                    } catch (android.content.ActivityNotFoundException ex) {
+//                        Toast.makeText(MyActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+//                    }
+//                    break;
+//                case R.id.returnLoginFg:
+//                    Intent nextPage = new Intent(getApplicationContext(), MainActivity.class);
+//                    startActivity(nextPage);
+//
+//        }
+//
+//    }
 }
--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,30 +12,33 @@ import android.widget.ImageButton;
 
 import android.os.Bundle;
 
-public class Home extends AppCompatActivity{
-
+public class Home extends AppCompatActivity implements View.OnClickListener  {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-//        EditText searchBarTextFieldHm = (EditText) findViewById(R.id.searchBarTextFieldHm);
-//        ImageButton searchButtonHm = (ImageButton) findViewById(R.id.searchButtonHm);
+        EditText searchBarTextFieldHm = (EditText) findViewById(R.id.searchBarTextFieldHm);
+        ImageButton searchButtonHm = (ImageButton) findViewById(R.id.searchButtonHm);
 //        ImageButton notificationButtonHm = (ImageButton) findViewById(R.id.notificationButtonHm);
-//        Button logOutButtonHm = (Button) findViewById((R.id.logOutButtonHm));
+        Button logOutButtonHm = (Button) findViewById((R.id.logOutButtonHm));
 
+        logOutButtonHm.setOnClickListener(this);
+        searchButtonHm.setOnClickListener(this);
+//        notificationButtonHm
     }
 
-//    @Override
-//    public void onClick(View v) {
-//        switch (v.getId()){
-//            case R.id.searchButtonHm:
-//                //search from database
-//                break;
-//            case R.id.notificationButtonHm:
-//                //notify user
-//                break;
-//
-//        }
-//    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.searchButtonHm:
+                //search from database
+                break;
+            case R.id.logOutButtonHm:
+                //notify user
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+
+        }
+    }
 }
